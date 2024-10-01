0004c6cad856b3d95bd1cad20d584c4e2f0ec57c
saadp21 <saadsajid.patel@student.sl.on.ca>
2024-10-01 12:49:00 -0400

    add static files

---
+++ src/main/java/comp31/saad_patel/SaadPatelApplication.java
@@ -0,0 +1,13 @@
+package comp31.saad_patel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaadPatelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaadPatelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/comp31/saad_patel/SaadPatelApplicationTests.java
@@ -0,0 +1,13 @@
+package comp31.saad_patel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaadPatelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
